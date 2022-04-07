72fcbd642e9e6856f07a50066e2011d18e651248
KENNY_NGOIE <mukonkolekngoie@gmail.com>
2022-04-07 18:14:03 +0200

    fist commit

---
+++ src/dummyData.js
@@ -0,0 +1,274 @@
+import { Style } from "@material-ui/icons";
+
+export const userData = [
+
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "Mar",
+      "Active User": 5000,
+    },
+    {
+      name: "Apr",
+      "Active User": 4000,
+    },
+    {
+      name: "May",
+      "Active User": 3000,
+    },
+    {
+      name: "Jun",
+      "Active User": 2000,
+    },
+    {
+      name: "Jul",
+      "Active User": 4000,
+    },
+    {
+      name: "Agu",
+      "Active User": 3000,
+    },
+    {
+      name: "Sep",
+      "Active User": 4000,
+    },
+    {
+      name: "Oct",
+      "Active User": 1000,
+    },
+    {
+      name: "Nov",
+      "Active User": 4000,
+    },
+    {
+      name: "Dec",
+      "Active User": 3000,
+    },
+  ];
+
+  export const productData = [
+    {
+      name: "Jan",
+      "Sales": 4000,
+    },
+    {
+      name: "Feb",
+      "Sales": 3000,
+    },
+    {
+      name: "Mar",
+      "Sales": 5000,
+    },
+  ];
+
+  export const userRows = [
+    {
+      id: 1,
+      username: "John Kitenge",
+      avatar:
+        "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "johnk@gmail.com",
+      status: "FNB",
+      account: "1266276267",
+      transaction: "R100.00",
+      views: "100"
+    },
+    {
+      id: 2,
+      username: "Kenny Ngoie",
+      avatar:
+        "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "kennyn@gmail.com",
+      status: "FNB",
+      account: "345276267",
+      transaction: "R100.00",
+      views: "100"
+    },
+    {
+      id: 3,
+      username: "Alinani",
+      avatar:
+      "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "alinani@gmail.com",
+      status: "Capitec",
+      account: "1234276267",
+      transaction: "R130.00",
+      views: "130"
+    },
+    {
+      id: 4,
+      username: "Njinu Kimani",
+      avatar:
+      "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "njinu.k@gmail.com",
+      status: "Nedbank",
+      account: "235456276267",
+      transaction: "R170.00",
+      views: "170"
+    },
+    {
+      id: 5,
+      username: "Remember Mabunda",
+      avatar:
+      "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "remember.m@gmail.com",
+      status: "Standard Bank",
+      account: "7026628291",
+      transaction: "R1210.00",
+      views: "1210"
+    },
+    {
+      id: 6,
+      username: "Mpumelelo Mpe",
+      avatar:
+      "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "mpumelelo@gmail.com",
+      status: "ABSA",
+      account: "43466333457",
+      transaction: "R120.00",
+      views: "120"
+    },
+    {
+      id: 7,
+      username: "Lenard Mak",
+      avatar:
+      "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "lenard.m@gmail.com",
+      status: "FNB",
+      account: "232323543545",
+      transaction: "R1220.00",
+      views: "1220"
+    },
+    {
+      id: 8,
+      username: "Promise Bong",
+      avatar:
+      "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "promise.b@gmail.com",
+      status: "Capitec",
+      account: "766210267",
+      transaction: "R420.00",
+      views: "420"
+    },
+    {
+      id: 9,
+      username: "Itumeleng Notsi",
+      avatar:
+      "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "itumeleng.n@gmail.com",
+      status: "Nedbank",
+      account: "766276267",
+      transaction: "R190.00",
+      views: "190"
+    },
+    {
+      id: 10,
+      username: "Joshua Selman",
+      avatar:
+      "https://resources.tidal.com/images/3e29689c/351e/4832/9c33/45619c5e46dc/750x750.jpg",
+      email: "joshua.s@gmail.com",
+      status: "Worldbank",
+      account: "766293767",
+      transaction: "R5000.00",
+      views: "5000"
+    },
+  ];
+
+  export const productRows = [
+    {
+      id: 1,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 2,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 3,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 4,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 5,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 6,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 7,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 8,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 9,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+    {
+      id: 10,
+      name: "Apple Airpods",
+      img:
+        "https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      stock: 123,
+      status: "active",
+      price: "$120.00",
+    },
+  ];
---
+++ src/pages/userList/UserList.jsx
@@ -0,0 +1,81 @@
+import "./userList.css";
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import { userRows } from "../../dummyData";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function UserList() {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+  
+  const columns = [
+    {
+      field: "user",
+      headerName: "Username",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img className="userListImg" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Bank",
+      width: 140,
+    },
+    {
+      field: "account",
+      headerName: "Account No.",
+      width: 170,
+    },
+    {
+      field: "transaction",
+      headerName: "Cash",
+      width: 130,
+    },
+    {
+      field: "views",
+      headerName: "Views",
+      width: 120,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="userListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="userList" text-color="white">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={5}
+        checkboxSelection
+      />
+    </div>
+  );
+}
