ac2b2642f01eedfb38d7f81a93fd61836eb68d00
KENNY_NGOIE <mukonkolekngoie@gmail.com>
2022-04-08 09:35:32 +0200

    more green

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -15,7 +15,7 @@ export default function Chart({ title, data, dataKey, grid }) {
       <h3 className="chartTitle">{title}</h3>
       <ResponsiveContainer width="100%" aspect={4 / 1}>
         <LineChart data={data}>
-          <XAxis dataKey="name" stroke="#00527c" />
+          <XAxis dataKey="name" stroke="#00E083" />
           <Line type="monotone" dataKey={dataKey} stroke="#00E083" />
           <Tooltip />
           {grid && <CartesianGrid stroke="#ffff" strokeDasharray="5 5" />}
